b3a83a919d4478a35412277fa33b363d37ac3b55
Ayush <8173997776aush@gmail.com>
2026-09-18 02:56:59 +0530

    Update CORS origin to use the correct deployed URL

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gamified-learning-zx3o.onrender.com'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gamified-learning-green.vercel.app'],
     credentials: true,              
 }))
 
