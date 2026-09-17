d46adefce94226aac47aa4e4f62d8b56e5f3482c
Ayush <8173997776aush@gmail.com>
2026-09-18 02:28:27 +0530

    added frontend  deployed link

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gamified-learning-zx3o.onrender.com'],
     credentials: true,              
 }))
 
